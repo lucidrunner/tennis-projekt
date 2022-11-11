26ae30e0503a27e5de66fe04a4ae9793bc1a1be6
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-11 15:23:53 +0100

    Added some smoooth transitions
    Also some cleanup on the borders of info-boxes & their gaps

--- sidebar.js
+++ sidebar.js
@@ -1,14 +1,14 @@
-function openNav(){
-    document.getElementById("sidenav").style.width = "200px";
-    let button = document.getElementById("sidenav-button");
-    button.style.right = "200px";
-    button.style.transform = "scaleX(-1)";
-    button.onclick = closeNav;
+function openNav() {
+  document.getElementById("sidenav").style.width = "200px";
+  let button = document.getElementById("sidenav-button");
+  button.style.right = "200px";
+  button.style.transform = "scaleX(-1)";
+  button.onclick = closeNav;
 }
-function closeNav(){
-    document.getElementById("sidenav").style.width = "0";
-    let button = document.getElementById("sidenav-button");
-    button.style.right = "3px";
-    button.style.transform = "scaleX(1)";
-    button.onclick = openNav;
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  let button = document.getElementById("sidenav-button");
+  button.style.right = "3px";
+  button.style.transform = "scaleX(1)";
+  button.onclick = openNav;
 }
