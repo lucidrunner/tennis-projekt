d86299605f142f3b19d1b7613397a3b43bd11719
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-13 23:55:37 +0100

    First pass of clamping / min / max checks to reduce media rules

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,7 @@
 function openNav() {
-  document.getElementById("sidenav").style.width = "200px";
+  document.getElementById("sidenav").style.width = "min(200px, 70%)";
   let button = document.getElementById("sidenav-button");
-  button.style.right = "200px";
+  button.style.right = "min(200px, 70%)";
   button.style.transform = "scaleX(-1)";
   button.onclick = closeNav;
 }
