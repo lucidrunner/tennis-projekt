dee12ee29ff6bd8b74b4e62e7bf7f6b2573a6242
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-15 14:12:16 +0100

    Switched some icons

--- header-nav.js
+++ header-nav.js
@@ -19,16 +19,16 @@ jQuery(function(){
 
         if(e.matches){
             $("#nav-index").text("").addClass("fa-solid fa-house fa-lg");
-            $("#nav-banor").text("").addClass("fa-solid fa-magnifying-glass fa-lg");
+            $("#nav-banor").text("").addClass("fa-solid fa-volleyball fa-lg");
             $("#nav-omkl").text("").addClass("fa-solid fa-restroom fa-lg");
-            $("#nav-bastu").text("").addClass("fa-solid fa-bath fa-lg");
+            $("#nav-bastu").text("").addClass("fa-solid fa-hot-tub-person fa-lg");
             $("#nav-spons").text("").addClass("fa-solid fa-medal fa-lg");
         }
         else{
             $("#nav-index").text(navIndexText).removeClass("fa-solid fa-house fa-lg");
-            $("#nav-banor").text(navBanorText).removeClass("fa-solid fa-magnifying-glass fa-lg");
+            $("#nav-banor").text(navBanorText).removeClass("fa-solid fa-volleyball fa-lg");
             $("#nav-omkl").text(navOmklText).removeClass("fa-solid fa-restroom fa-lg");
-            $("#nav-bastu").text(navBastuText).removeClass("fa-solid fa-bath fa-lg");
+            $("#nav-bastu").text(navBastuText).removeClass("fa-solid fa-hot-tub-person fa-lg");
             $("#nav-spons").text(navSponsText).removeClass("fa-solid fa-medal fa-lg");;
         }
     }
