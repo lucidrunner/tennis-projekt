64f7e6b5c42282654d5c2bb04a340182aa1198a4
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-15 13:52:49 +0100

    Icon menu added
    Jquery + jsfconfig added to the project to do it

--- header-nav.js
+++ header-nav.js
@@ -1,8 +1,12 @@
 
 
-$(document).ready(function(){
+jQuery(function(){
     const mqMobile = window.matchMedia('(max-width: 425px)');
     const navIndexText = "Om STF"; 
+    const navBanorText = "Banor";
+    const navOmklText = "Omklädningsrum";
+    const navBastuText = "Bastu";
+    const navSponsText = "Våra Sponsorer";
 
 
     mqMobile.addEventListener('change', mqHandler);
@@ -14,10 +18,18 @@ $(document).ready(function(){
         e.matches ? 'small' : 'not small');
 
         if(e.matches){
-            $("#nav-index").text("Om");
+            $("#nav-index").text("").addClass("fa-solid fa-house fa-lg");
+            $("#nav-banor").text("").addClass("fa-solid fa-magnifying-glass fa-lg");
+            $("#nav-omkl").text("").addClass("fa-solid fa-restroom fa-lg");
+            $("#nav-bastu").text("").addClass("fa-solid fa-bath fa-lg");
+            $("#nav-spons").text("").addClass("fa-solid fa-medal fa-lg");
         }
         else{
-            $("#nav-index").text(navIndexText);
+            $("#nav-index").text(navIndexText).removeClass("fa-solid fa-house fa-lg");
+            $("#nav-banor").text(navBanorText).removeClass("fa-solid fa-magnifying-glass fa-lg");
+            $("#nav-omkl").text(navOmklText).removeClass("fa-solid fa-restroom fa-lg");
+            $("#nav-bastu").text(navBastuText).removeClass("fa-solid fa-bath fa-lg");
+            $("#nav-spons").text(navSponsText).removeClass("fa-solid fa-medal fa-lg");;
         }
     }
 
