2f8d256e69782e95bf5d4720443aeaf464160f3a
Björn Rundquist <50362568+lucidrunner@users.noreply.github.com>
2022-11-16 16:31:04 +0100

    Added time validation to the booking site

--- booking.js
+++ booking.js
@@ -1,34 +1,74 @@
 function validateForm(identifier) {
+    let passed = false;
     switch (identifier) {
         case "bastu":
-            return validateBastu();
+            passed = validateBastu();
+            break;
         case "omkl":
-            return validateOmkl();
+            passed = validateOmkl();
+            break;
         case "bana":
-            return validateBana();
+            passed = validateBana();
+            break;
         default:
             alert("Incorrect form submitted, please contact the site owner.");
-            return false;
+            passed = false;
+            break;
     }
+    if(passed){
+        alert("BOOKED");
+    }
+
+    return passed;
 }
 
 function validateBastu() {
-
-    return true;
+    return validateTime() && validatePersonalInfo();
 }
 
 function validateBana() {
-    return true;
+    return validateTime() && validatePersonalInfo();
 }
 
 function validateOmkl() {
-    return true;
+    return validateTime() && validatePersonalInfo();
 }
 
 function validateTime() {
+    const bookedDate = $("#time-date").val();
+    let dateEntered = new Date(bookedDate)
+    dateEntered.setHours(getTime());
+    let today = new Date();
+    console.log(dateEntered);
+    if(today >= dateEntered)
+    {
+        alert("Failed with booking - starting time has already passed.");
+        return false;
+    }
     return true;
 }
 
 function validatePersonalInfo() {
     return true;
+}
+
+function getTime()
+{
+    let timeSlot = $("#time-slot").val();
+    console.log(timeSlot);
+    timeSlot = parseInt(timeSlot);
+    console.log(timeSlot);
+    //Edge cases for our morning / afternoon passes
+    if(timeSlot == 7)
+    {
+        return 8;
+    }
+    else if(timeSlot == 8)
+    {
+        return 13;
+    }
+
+    // Our starting time is 8 and we have two hour blocks to consider
+    //We only allow booking before the time has started to avoid confusion too
+    return 8 + (timeSlot - 1) * 2;
 }
