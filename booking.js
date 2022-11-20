e690264fab33b0990ed600af4089bda69d7439a6
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-20 16:58:12 +0100

    Eslint possibly setup
    Some weird errors popping up atm so probably works (also configured prettier)

--- booking.js
+++ booking.js
@@ -1,83 +1,79 @@
 //Restrict calendar to minimum of today (and also set the default date to today)
-window.onload = function(){
-    let datePicker = document.getElementById("time-date");
-    const today = new Date();
-    const dateAsString = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-    datePicker.value = dateAsString;
-    datePicker.min = dateAsString;
+window.onload = function () {
+  let datePicker = document.getElementById("time-date");
+  const today = new Date();
+  const dateAsString =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  datePicker.value = dateAsString;
+  datePicker.min = dateAsString;
 };
 
 function validateForm(identifier) {
-    let passed = false;
-    switch (identifier) {
-        case "bastu":
-            passed = validateBastu();
-            break;
-        case "omkl":
-            passed = validateOmkl();
-            break;
-        case "bana":
-            passed = validateBana();
-            break;
-        default:
-            alert("Incorrect form submitted, please contact the site owner.");
-            passed = false;
-            break;
-    }
-    if(passed){
-        alert("BOOKED");
-    }
+  let passed = false;
+  switch (identifier) {
+    case "bastu":
+      passed = validateBastu();
+      break;
+    case "omkl":
+      passed = validateOmkl();
+      break;
+    case "bana":
+      passed = validateBana();
+      break;
+    default:
+      alert("Incorrect form submitted, please contact the site owner.");
+      passed = false;
+      break;
+  }
+  if (passed) {
+    alert("BOOKED");
+  }
 
-    return passed;
+  return passed;
 }
 
 function validateBastu() {
-    return validateTime() && validatePersonalInfo();
+  return validateTime() && validatePersonalInfo();
 }
 
 function validateBana() {
-    return validateTime() && validatePersonalInfo();
+  return validateTime() && validatePersonalInfo();
 }
 
 function validateOmkl() {
-    return validateTime() && validatePersonalInfo();
+  return validateTime() && validatePersonalInfo();
 }
 
 function validateTime() {
-    const bookedDate = $("#time-date").val();
-    let dateEntered = new Date(bookedDate)
-    dateEntered.setHours(getTime());
-    let today = new Date();
-    console.log(dateEntered);
-    if(today >= dateEntered)
-    {
-        alert("Failed with booking - starting time has already passed.");
-        return false;
-    }
-    return true;
+  const bookedDate = $("#time-date").val();
+  let dateEntered = new Date(bookedDate);
+  dateEntered.setHours(getTime());
+  let today = new Date();
+  console.log(dateEntered);
+  if (today >= dateEntered) {
+    alert("Failed with booking - starting time has already passed.");
+    return false;
+  }
+  return true;
 }
 
 function validatePersonalInfo() {
-    return true;
+  return true;
 }
 
-function getTime()
-{
-    let timeSlot = $("#time-slot").val();
-    console.log(timeSlot);
-    timeSlot = parseInt(timeSlot);
-    console.log(timeSlot);
-    //Edge cases for our morning / afternoon passes
-    if(timeSlot == 7)
-    {
-        return 8;
-    }
-    else if(timeSlot == 8)
-    {
-        return 13;
-    }
+function getTime() {
+  let timeSlot = $("#time-slot").val();
+  console.log(timeSlot);
+  timeSlot = parseInt(timeSlot);
+  console.log(timeSlot);
+  //Edge cases for our morning / afternoon passes
+  if (timeSlot == 7) {
+    return 8;
+  } else if (timeSlot == 8) {
+    return 13;
+  }
 
-    // Our starting time is 8 and we have two hour blocks to consider
-    //We only allow booking before the time has started to avoid confusion too
-    return 8 + (timeSlot - 1) * 2;
+  // Our starting time is 8 and we have two hour blocks to consider
+  //We only allow booking before the time has started to avoid confusion too
+  return 8 + (timeSlot - 1) * 2;
 }
--- header-nav.js
+++ header-nav.js
@@ -1,40 +1,40 @@
-
-
 jQuery(function () {
-    const mqMobile = window.matchMedia('(max-width: 425px)');
-    const navIndexText = "Om STF";
-    const navBanorText = "Banor";
-    const navOmklText = "Omklädningsrum";
-    const navBastuText = "Bastu";
-    const navSponsText = "Våra Sponsorer";
+  const mqMobile = window.matchMedia("(max-width: 425px)");
+  const navIndexText = "Om STF";
+  const navBanorText = "Banor";
+  const navOmklText = "Omklädningsrum";
+  const navBastuText = "Bastu";
+  const navSponsText = "Våra Sponsorer";
 
+  mqMobile.addEventListener("change", mqHandler);
 
-    mqMobile.addEventListener('change', mqHandler);
+  mqHandler(mqMobile);
 
-    mqHandler(mqMobile)
+  function mqHandler(e) {
+    console.log(e.matches ? "small" : "not small");
 
-    function mqHandler(e) {
-        console.log(
-            e.matches ? 'small' : 'not small');
-
-        if (e.matches) {
-            // Without jquery
-            // document.getElementById("nav-index").textContent = "";
-            // document.getElementById("nav-index").classList.add("fa-solid", "fa-volleyball", "fa-lg");
-            //With Jquery
-            $("#nav-index").text("").addClass("fa-solid fa-house fa-lg");
-            $("#nav-banor").text("").addClass("fa-solid fa-volleyball fa-lg");
-            $("#nav-omkl").text("").addClass("fa-solid fa-restroom fa-lg");
-            $("#nav-bastu").text("").addClass("fa-solid fa-hot-tub-person fa-lg");
-            $("#nav-spons").text("").addClass("fa-solid fa-medal fa-lg");
-        }
-        else {
-            $("#nav-index").text(navIndexText).removeClass("fa-solid fa-house fa-lg");
-            $("#nav-banor").text(navBanorText).removeClass("fa-solid fa-volleyball fa-lg");
-            $("#nav-omkl").text(navOmklText).removeClass("fa-solid fa-restroom fa-lg");
-            $("#nav-bastu").text(navBastuText).removeClass("fa-solid fa-hot-tub-person fa-lg");
-            $("#nav-spons").text(navSponsText).removeClass("fa-solid fa-medal fa-lg");;
-        }
+    if (e.matches) {
+      // Without jquery
+      // document.getElementById("nav-index").textContent = "";
+      // document.getElementById("nav-index").classList.add("fa-solid", "fa-volleyball", "fa-lg");
+      //With Jquery
+      $("#nav-index").text("").addClass("fa-solid fa-house fa-lg");
+      $("#nav-banor").text("").addClass("fa-solid fa-volleyball fa-lg");
+      $("#nav-omkl").text("").addClass("fa-solid fa-restroom fa-lg");
+      $("#nav-bastu").text("").addClass("fa-solid fa-hot-tub-person fa-lg");
+      $("#nav-spons").text("").addClass("fa-solid fa-medal fa-lg");
+    } else {
+      $("#nav-index").text(navIndexText).removeClass("fa-solid fa-house fa-lg");
+      $("#nav-banor")
+        .text(navBanorText)
+        .removeClass("fa-solid fa-volleyball fa-lg");
+      $("#nav-omkl")
+        .text(navOmklText)
+        .removeClass("fa-solid fa-restroom fa-lg");
+      $("#nav-bastu")
+        .text(navBastuText)
+        .removeClass("fa-solid fa-hot-tub-person fa-lg");
+      $("#nav-spons").text(navSponsText).removeClass("fa-solid fa-medal fa-lg");
     }
-
-}); 
+  }
+});
